5fcff9e2d55067fd2d27a4c7221abc933f0e336d
mariusz.wojcik <mariusz.wojcik@asseco.pl>
2021-02-06 21:57:50 +0100

    Domain Explorers init

---
+++ src/main/java/mw/domainexplorers/DomainexplorersApplication.java
@@ -0,0 +1,13 @@
+package mw.domainexplorers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DomainexplorersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DomainexplorersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mw/domainexplorers/DomainexplorersApplicationTests.java
@@ -0,0 +1,13 @@
+package mw.domainexplorers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DomainexplorersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
